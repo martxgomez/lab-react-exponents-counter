ab3935040b6df894ec574a569e7a09383ebc4800
Marta Gómez <martagomezmartinez95@gmail.com>
2024-11-19 20:55:26 +0100

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,24 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
 
 
 function App () {
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+ 
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
 
 
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter count={count}/>
-      <button className="counter-button" onClick={setCount(count-1)}>-</button>
-      <button className="counter-button" onClick={setCount(count+1)}>+</button>
+      <Counter count={count} decrement={decrement} increment={increment}/>
+     
 
       <br />
       <h2><em>Exponents</em></h2>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
 
-const Counter = (props) => {
- 
+const Counter = ({count, decrement, increment}) => {
 
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
 
   return (
     <div className="counter-container">
       <p className="counter-value">{count}</p>
-    
+      <button className="counter-button" onClick={decrement}>-</button>
+      <button className="counter-button" onClick={increment}>+</button>
     </div>
   );
 };
